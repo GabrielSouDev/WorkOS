d3580c987ead21d45955cd0199b5f146b530d34c
Gabriel Souza <61925245+GabrielSouDev@users.noreply.github.com>
2025-01-31 17:58:26 -0300

    Criação da estrutura do site client

--- WorkOS.AppHost/Program.cs
+++ WorkOS.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.WorkOS_API>("workos-api");
 
+builder.AddProject<Projects.WorkOS_Client>("workos-client");
+
 builder.Build().Run();
